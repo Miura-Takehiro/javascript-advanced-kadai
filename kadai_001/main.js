771d29d0a4e8846fc7bf944f97eb849cacc980a8
Miura-Takehiro <126331839+Miura-Takehiro@users.noreply.github.com>
2024-04-07 17:18:24 +0900

    現在のタイプ数を表示しよう

---
+++ kadai_001/main.js
@@ -0,0 +1,132 @@
+// ゲーム開始前は空欄にしておく
+let untyped = '';
+let typed = '';
+let score = 0;
+let typingCount = 0; // タイピングした文字数をカウントする変数を追加
+
+// 必要なHTML要素の取得
+const untypedfield = document.getElementById('untyped');
+const typedfield = document.getElementById('typed');
+const wrap = document.getElementById('wrap');
+const start = document.getElementById('start');
+const count = document.getElementById('count');
+const typingCountElement = document.getElementById('typingCount'); // タイピング数の表示要素を取得
+
+// テキストを格納する配列
+const textLists = [
+  'Hello World',
+  'This is my App',
+  'How are you?',
+  'Today is sunny', 'I love JavaScript!', 'Good morning',
+  'I am Japanese', 'Let it be', 'Samurai',
+  'Typing Game', 'Information Technology',
+  'I want to be a programmer', 'What day is today?',
+  'I want to build a web app', 'Nice to meet you',
+  'Chrome Firefox Edge Safari', 'machine learning',
+  'Brendan Eich', 'John Resig', 'React Vue Angular',
+  'Netscape Communications', 'undefined null NaN',
+  'Thank you very much', 'Google Apple Facebook Amazon',
+  'ECMAScript', 'console.log', 'for while if switch',
+  'var let const', 'Windows Mac Linux iOS Android',
+  'programming'
+];
+
+// ランダムなテキストを表示
+const createText = () => {
+  // タイピング数をクリア
+  typingCount = 0; // タイピング数を0にリセット
+  typingCountElement.textContent = ''; // タイピング数の表示もクリア
+  // 正タイプした文字列をクリア
+  typed = '';
+  typedfield.textContent = typed;
+  // 配列のインデックス数からランダムな数値を生成する
+  let random = Math.floor(Math.random() * textLists.length);
+  untyped = textLists[random];
+  untypedfield.textContent = untyped;
+};
+
+// キー入力の判定
+const keyPress = e => {
+  // 誤タイプの場合
+  if (e.key !== untyped.substring(0, 1)) {
+    wrap.classList.add('mistyped');
+    // 100ms後に背景色を元に戻す
+    setTimeout(() => {
+      wrap.classList.remove('mistyped');
+    }, 100);
+    return;
+  }
+
+  // 正タイプの場合
+  score++;
+  typingCount++; // タイピング数をインクリメント
+  typed += untyped.substring(0, 1);
+  untyped = untyped.substring(1);
+  typedfield.textContent = typed;
+  untypedfield.textContent = untyped;
+  // タイピングした文字数をカウント
+  typingCountElement.textContent = typed.length;
+
+  // テキストがなくなったら新しいテキストを表示
+  if (untyped === '') {
+    createText();
+  }
+};
+
+// タイピングスキルのランクを判定
+const rankCheck = score => {
+  let text = '';
+
+  if (score < 100) {
+    text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
+  } else if (score < 200) {
+    text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;
+  } else if (score < 300) {
+    text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;
+  } else if (score >= 300) {
+    text = `あなたのランクはSです。\nおめでとうございます!`;
+  }
+
+  return `${score}文字打てました!\n${text}\n【OK】リトライ / 【キャンセル】終了`;
+};
+
+// ゲームを終了
+const gameOver = id => {
+  clearInterval(id);
+  timeout(); // タイムアップを表示
+    const result = confirm(rankCheck(score));
+  if (result == true) {
+       window.location.reload();
+  }
+
+};
+
+// タイムアウトを表示
+const timeout = () => {
+  untypedfield.textContent = 'タイムアップ‼';
+};
+
+// カウントダウンタイマー
+const timer = () => {
+  let time = count.textContent;
+
+  const id = setInterval(() => {
+    time--;
+    count.textContent = time;
+
+    if (time <= 0) {
+      gameOver(id);
+    }
+  }, 1000);
+};
+
+
+// ゲームスタート時の処理
+start.addEventListener('click', () => {
+  timer();
+  createText();
+  start.style.display = 'none';
+  document.addEventListener('keypress', keyPress);
+});
+
+untypedfield.textContent = 'スタートボタンで開始';
